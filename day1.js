4b093a33b935f5e9b35db8002d13c9742a71794c
pokala57 <pokalanani2000@gmail.com>
2025-03-29 11:44:13 +0530

    js into , variables,functions

---
+++ day1.js
@@ -0,0 +1,15 @@
+var name = "rohith";
+let  age = 22;
+const dob= "30-06-2001"
+function displayinfo(a,b){
+//    let a =10 ,b=20;
+// let details={"name":"rohith","age":22}
+// let name = null;
+//     console.log(typeof details)
+   let var1 = true,var2=true;
+
+   return var1&&var2;
+}
+let returnvalue = displayinfo(10,20);
+console.log(returnvalue)
+
