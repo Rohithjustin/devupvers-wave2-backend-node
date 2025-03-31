848d376c66ed8c611fb1bb3abc6d17889a57a0d5
pokala57 <pokalanani2000@gmail.com>
2025-03-31 21:36:01 +0530

    day2 class

--- day1.js
+++ day1.js
@@ -1,15 +1,43 @@
 var name = "rohith";
 let  age = 22;
 const dob= "30-06-2001"
+let isrohith = true
+let values={"age":11,"name":"rohith"}
+let time=null
+let day;
+
+
+console.log(age)
+
+
+ function add(a,b)
+
+ {
+   let var1 = true,var2=true;
+   
+   console.log(var1&&var2)
+   return a+b
+ }
+ function sub(a,b)
+
+ {
+  console.log(a-b);
+ }
+
+
+console.log(add(20,20))
+
+
 function displayinfo(a,b){
 //    let a =10 ,b=20;
 // let details={"name":"rohith","age":22}
 // let name = null;
-//     console.log(typeof details)
-   let var1 = true,var2=true;
+//   console.log(typeof details)
+//    let var1 = true,var2=true;
 
-   return var1&&var2;
+//    return var1&&var2;
+// console.log(name)
 }
 let returnvalue = displayinfo(10,20);
-console.log(returnvalue)
+// console.log(returnvalue)
 
