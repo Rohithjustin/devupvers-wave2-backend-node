80c59edfc920ebcb75cc4d3528d2afe66cf9640b
Sree-1405 <hemareddybuchupalli@gmail.com>
2025-04-05 21:56:06 +0530

    save changes

---
+++ reduceday6.js
@@ -0,0 +1,6 @@
+let fruits = ["apple","banana","apple","orange","banana","apple"];
+let fruitCount = fruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0) + 1;
+  return acc;
+}, {});
+console.log(fruitCount);
